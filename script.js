d3f8b8bb4dcfc8ccfdc6b82f4157f4f58de79bc2
TheBims7 <farizbimaprayoga@gmail.com>
2025-07-21 22:25:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,18 +34,7 @@ function type() {
 
 document.addEventListener("DOMContentLoaded", type);
 
-let menuList = document.getElementById("menuList")
-menuList.style.maxHeight = "0px";
-
 //navbar
-// function toggleMenu(){
-//     if(menuList.style.maxHeight == "0px"){
-//         menuList.style.maxHeight = "300px";
-//     }
-//     else{
-//         menuList.style.maxHeight = "0px";
-//     }
-// }
 function toggleMenu(){
     const menuList = document.getElementById("menuList");
     const header = document.querySelector("header");
